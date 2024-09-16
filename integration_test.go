b60d20708e8b6bc2ee7522f68ec0f7022f965222
Мухутдинов Артур <arturmuxutdinov@gmail.com>
2024-09-16 17:46:55 +0300

    Update integration_test.go

--- integration_test.go
+++ integration_test.go
@@ -11,33 +11,34 @@ import (
     "github.com/stretchr/testify/assert"
     "github.com/testcontainers/testcontainers-go"
     "github.com/testcontainers/testcontainers-go/wait"
+    "github.com/testcontainers/testcontainers-go/modules/postgres"
     _ "github.com/lib/pq"
 )
 
 func TestPostgresContainer(t *testing.T) {
     ctx := context.Background()
 
-dbName := "users"
-dbUser := "user"
-dbPassword := "password"
-
-postgresContainer, err := postgres.Run(ctx,
-    "docker.io/postgres:16-alpine",
-    postgres.WithDatabase(dbName),
-    postgres.WithUsername(dbUser),
-    postgres.WithPassword(dbPassword),
-    testcontainers.WithWaitStrategy(
-        wait.ForLog("database system is ready to accept connections").
-            WithOccurrence(2).
-            WithStartupTimeout(5*time.Second)),
-)
-defer func() {
-    if err := testcontainers.TerminateContainer(postgresContainer); err != nil {
-        log.Printf("failed to terminate container: %s", err)
+    dbName := "users"
+    dbUser := "user"
+    dbPassword := "password"
+    
+    postgresContainer, err := postgres.Run(ctx,
+        "docker.io/postgres:16-alpine",
+        postgres.WithDatabase(dbName),
+        postgres.WithUsername(dbUser),
+        postgres.WithPassword(dbPassword),
+        testcontainers.WithWaitStrategy(
+            wait.ForLog("database system is ready to accept connections").
+                WithOccurrence(2).
+                WithStartupTimeout(5*time.Second)),
+    )
+    defer func() {
+        if err := testcontainers.TerminateContainer(postgresContainer); err != nil {
+            log.Printf("failed to terminate container: %s", err)
+        }
+    }()
+    if err != nil {
+        log.Printf("failed to start container: %s", err)
+        return
     }
-}()
-if err != nil {
-    log.Printf("failed to start container: %s", err)
-    return
-}
 }
